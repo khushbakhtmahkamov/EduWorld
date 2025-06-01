14a47036765332582693cd9cb8780ac548cc81e8
Joeknow03 <jahongirakiljanov@gmail.com>
2025-06-01 19:30:20 +0500

    EduWolrd-5 Language model update

--- src/main/java/org/example/service/UserLanguage.java
+++ src/main/java/org/example/service/UserLanguage.java
@@ -1,5 +1,18 @@
 package org.example.service;
 
+import java.util.List;
+
 public interface UserLanguage {
+    void setLanguage(String language);
+    String getLanguage();
+
+    void setLanguages(List<String> languages);
+    List<String> getLanguages();
+
+    boolean addLanguage(String language);
+    boolean removeLanguage(String language);
+
+
+
 
 }
