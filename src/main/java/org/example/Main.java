9a4cfd826d33cb14069b0284469676ba187004cc
Shafrann <safikasodieva79@gmail.com>
2025-06-04 15:55:11 +0500

    EduWorld-9 Create Subject Service and add default functions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import org.example.model.Language;
+import org.example.model.Subject;
 import org.example.model.User;
+import org.example.service.SubjectService;
+import org.example.service.SubjectServiceImpl;
 import org.example.service.UserService;
 import org.example.service.UserServiceImpl;
 
@@ -9,12 +12,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         UserService userService = new UserServiceImpl();
         Language en = new Language("English", "en", true);
         Language ru = new Language("Russian", "ru", true);
         User user1 = new User("User1", 22, "test@maile", "122", en);
         userService.addUser(user1);
-
 
         User user2 = new User("User2", 23, "test@maile1", "122", ru);
         userService.addUser(user2);
@@ -23,10 +26,9 @@ public class Main {
             System.out.println("NameRU: " + user.getName() +
                     ", Age: " + user.getAge() +
                     ", Email: " + user.getEmail() +
-                    ", Password: " + user.getPassword()+
+                    ", Password: " + user.getPassword() +
                     ", Language: " + user.getLanguage().getName());
         }
-
 
         userService.removeUserByName(user1.getName());
 
@@ -41,13 +43,44 @@ public class Main {
         }
 
         System.out.println("After removing " + user1.getName() + ":");
-
         List<User> usersAfterRemove = userService.getUsers();
         for (User user : usersAfterRemove) {
             System.out.println("Name: " + user.getName() +
                     ", Age: " + user.getAge() +
                     ", Email: " + user.getEmail() +
                     ", Password: " + user.getPassword());
+        }
+
+        SubjectService subjectService = new SubjectServiceImpl();
+
+        Subject math = new Subject();
+        math.setTitle("Mathematics");
+        math.setCode("MATH101");
+        math.setCredits(3);
+        math.setDescription("An introduction to mathematical concepts.");
+
+        Subject history = new Subject();
+        history.setTitle("History");
+        history.setCode("HIST101");
+        history.setCredits(3);
+        history.setDescription("An overview of world history.");
+
+        subjectService.addSubject(math);
+        subjectService.addSubject(history);
+
+        List<Subject> allSubjects = subjectService.getSubjects();
+        System.out.println("All Subjects:");
+        for (Subject subject : allSubjects) {
+            System.out.println(subject);
+        }
+
+
+        subjectService.removeSubjectByCode("MATH101");
+        System.out.println("After removing Mathematics:");
+
+        List<Subject> remainingSubjects = subjectService.getSubjects();
+        for (Subject subject : remainingSubjects) {
+            System.out.println(subject);
         }
     }
 }
